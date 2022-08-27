5a7fadef326130ea9d21b1fc32a682cad1e5bd82
hoxas <hoxas@live.com>
2022-08-27 16:54:55 -0300

    Ex feitos pela prof

---
+++ exercicios/alice-almeida/conteúdo/condicionais.js
@@ -0,0 +1,22 @@
+let regiao = 'centro-oeste'
+
+switch (regiao.toLowerCase()) {
+    case 'nordeste':
+        console.log('possui 9 estados');
+        break;
+    case 'norte':
+        console.log('possui 7 estados');
+        break;
+    case 'centro-oeste':
+        console.log('possui 3 estados e DF');
+        break;
+    case 'sudeste':
+        console.log('possui 4 estados');
+        break;
+    case 'sul':
+        console.log('possui 3 estados');
+        break;
+    default:
+        console.log('estado inválido')
+}
+
---
+++ exercicios/alice-almeida/conteúdo/funcoes.js
@@ -0,0 +1,3 @@
+function calcular() {
+    console.log('Calculando...')
+}
