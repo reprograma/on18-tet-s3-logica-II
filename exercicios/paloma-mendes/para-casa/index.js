15b93f70b6780135869311627574e650590ca3b5
palomamendesr <palomamendesr@gmail.com>
2022-09-02 14:32:22 -0300

    Fiz atualização de algumas coisas que não estavam funcionando no código

--- exercicios/paloma-mendes/para-casa/index.js
+++ exercicios/paloma-mendes/para-casa/index.js
@@ -1,41 +1,44 @@
-let cliente = [];
 let clienteLilit = [
-    { produto: 'Bolsa pequena', valor: 49.0 },
-    { produto: 'Cinto preto', valor: 62.0 },
-    { produto: 'Jaqueta Jeans', valor: 350.0 },
-    { produto: 'Calça preta', valor: 290.0 },
-    { produto: 'Blusa simples', valor: 35.0 },
-    { produto: 'Calça jeans clara', valor: 130.0 },
-    { produto: 'Blusa preta gola alta', valor: 60.0 },
-    { produto: 'Short verde canelado', valor: 80.0 },
-    { produto: 'Salto agulha 39', valor: 250.0 },
-    { produto: 'Tênis casual preto', valor: 120.0 },
-    { produto: 'meia calça transparente', valor: 80.0 },
-  ]
+  { produto: 'Bolsa pequena', valor: 49.0 },
+  { produto: 'Cinto preto', valor: 62.0 },
+  { produto: 'Jaqueta Jeans', valor: 350.0 },
+  { produto: 'Calça preta', valor: 290.0 },
+  { produto: 'Blusa simples', valor: 35.0 },
+  { produto: 'Calça jeans clara', valor: 130.0 },
+  { produto: 'Blusa preta gola alta', valor: 60.0 },
+  { produto: 'Salto agulha 39', valor: 250.0 },
+  { produto: 'Tênis casual preto', valor: 120.0 },
+  { produto: 'meia calça transparente', valor: 80.0 },
+]
 
 let clientePaloma = [
-    { produto: 'Jaqueta Jeans', valor: 350.0 },
-    { produto: 'Tênis casual preto', valor: 120.0 },
-    { produto: 'meia calça transparente', valor: 80.0 },
-    { produto: 'Blusa simples', valor: 35.0 },
-    { produto: 'Blusa preta gola alta', valor: 60.0 },
-  ]
+  { produto: 'Jaqueta Jeans', valor: 35.0 },
+  { produto: 'Tênis casual preto', valor: 12.0 },
+  { produto: 'meia calça transparente', valor: 80.0 },
+  { produto: 'Blusa simples', valor: 35.0 },
+  { produto: 'Blusa preta gola alta', valor: 60.0 },
+  { produto: 'Salto agulha 39', valor: 25.0 },
+  { produto: 'Calça jeans clara', valor: 13.0 },
+  { produto: 'meia calça transparente', valor: 8.0 },
+  { produto: 'Bolsa pequena', valor: 9.0 },
+  { produto: 'Blusa preta gola alta', valor: 6.0 },
+  { produto: 'Short verde canelado', valor: 80.0 },
+]
 
 let clienteLeo = [
-    { produto: 'Cinto preto', valor: 62.0 },
-    { produto: 'Jaqueta Jeans', valor: 350.0 },
-    { produto: 'Blusa simples', valor: 35.0 },
-  ]
+  { produto: 'Cinto preto', valor: 60.0 },
+  { produto: 'Jaqueta Jeans', valor: 400.0 },
+  { produto: 'Blusa simples', valor: 100.0 },
+]
 
-function SomarValorCarrinho(parametro_cliente) {
+function somarValorCarrinho(parametro_cliente, formaPagamento) {
   let valorTotal = 0
   let desconto = 0
   let descontoTotal = 0
   let valorFinal = 0
   let cupom = 0
-  let descontoPIX = 0
-  clientePagamento = 0
-  let prodruto = 0
+  let descontoPagamento = 0
+  let produto = 0
   let data = new Date();
   let dia = data.getDate();
   let mes = data.getMonth() + 1;
@@ -43,9 +46,10 @@ function SomarValorCarrinho(parametro_cliente) {
   let hora = data.getHours();
   let minuto = data.getMinutes();
   let segundo = data.getSeconds();
-  let dataHora = `${hora}:${minuto}:${segundo} ${dia}/${mes}/${ano} `
-  
-  for (let produto of clientePaloma) {
+  let dataHora = `${hora}:${minuto}:${segundo} ${dia}/${mes}/${ano}`
+  let totalPecas = parametro_cliente.length;
+
+  for (produto of parametro_cliente) {
     valorTotal = valorTotal += produto.valor;
 
     if (produto.valor >= 60.0 && produto.valor < 100.0) {
@@ -59,20 +63,25 @@ function SomarValorCarrinho(parametro_cliente) {
       desconto = produto.valor * 0.5
       descontoTotal = desconto
     }
-    if (clientePagamento == 'pix' || clientePagamento == 'à vista') {
-      valorTotal = descontoTotal * 0.05
+    if (formaPagamento == 'pix' || formaPagamento == 'à vista') {
+      descontoPagamento = descontoTotal * 0.05
     }
-    if (produto.valor >= 10.0 || produto.valor >= 800.0) {
-      cupom = 50.0
+    if (totalPecas > 10.0 || valorFinal > 800.0) {
+      cupom = 50
     }
-    valorFinal = (valorTotal - descontoTotal);
+    valorFinal = (valorTotal - descontoTotal - descontoPagamento);
   }
-  console.table(clientePaloma)
-  console.log('-----------------------------------')
+  console.log('-----------------SUAS COMPRAS-----------------')
+  console.table(parametro_cliente)
   console.log('Valor das suas compras R$: ' + valorTotal)
-  console.log('Valor de desconto R$: '  + descontoTotal)
-  console.log('Você possui um total de '+ cupom + ' reais de desconto para próxima compra!')
+  console.log('Valor de desconto R$: ' + descontoTotal)
+  console.log('Desconto pela forma de pagamento R$: ' + descontoPagamento.toFixed(2))
+  console.log('Valor total a pagar R$: ' + valorFinal)
+  console.log('Você possui um cupom de R$: ' + cupom + ' reais de desconto para próxima compra!')
   console.log(dataHora)
+  
 }
 
-SomarValorCarrinho(cliente.clientePaloma)
+somarValorCarrinho(clienteLilit, 'pix')
+somarValorCarrinho(clientePaloma, 'credito')
+somarValorCarrinho(clienteLeo, 'à vista')
